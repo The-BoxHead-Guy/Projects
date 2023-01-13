5c4c43e02660e9a208177ef0414572e0a557b7fc
Jhon Alessandro <astralgmd@gmail.com>
2023-01-13 18:41:14 -0400

    CSS changes, and JS function additions

---
+++ 1-Color-Flipper/My-own-version/Project/js/app.js
@@ -0,0 +1,11 @@
+const color = ['red', 'blue', 'orange'];  
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    
+    /* alert('The button has been clicked'); */
+    // There will be the function which will change the color of the background
+
+    console.log('Clicked');
+
+})
