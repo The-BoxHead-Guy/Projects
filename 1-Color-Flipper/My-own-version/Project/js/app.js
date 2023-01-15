f43fc84b3cce529b02c9d71448064006f8efaeb6
Jhon Alessandro <astralgmd@gmail.com>
2023-01-14 21:42:01 -0400

    modifying app.js, styles.css and index.html

--- 1-Color-Flipper/My-own-version/Project/js/app.js
+++ 1-Color-Flipper/My-own-version/Project/js/app.js
@@ -1,11 +1,27 @@
-const color = ['red', 'blue', 'orange'];  
+/* Global Variables */
+const elements = ['red', 'blue', 'orange', 'beige', 'grey', 'purple', 'pink', 'green', 'lightblue', 'black', 'white', 'brown', 'yellow'];  
 
 const btn = document.getElementById('btn');
+const titleContent = document.querySelector('.background-color');
+const titleColor = document.getElementById('background-color');
+
+// Changing background color
+const container = document.getElementById('container');
+
 btn.addEventListener('click', function () {
     
-    /* alert('The button has been clicked'); */
+    const randomNumber = getRandomNumber();
+
     // There will be the function which will change the color of the background
+    
+    container.style.backgroundColor = elements[randomNumber];
 
-    console.log('Clicked');
-
+    titleContent.textContent = elements[randomNumber];
+    titleColor.style.color = elements[randomNumber];
 })
+
+// Creating the randonNumber function
+
+function getRandomNumber() {
+    return nRandom = Math.floor(Math.random() * elements.length);
+}
